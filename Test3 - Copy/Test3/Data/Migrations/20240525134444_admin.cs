d629eace7126c912bca57ac183fa574358c6a62f
ST10396745 <127109921+ST10396745@users.noreply.github.com>
2024-06-23 21:10:07 +0200

    Adding Project

---
+++ Test3 - Copy/Test3/Data/Migrations/20240525134444_admin.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Test3.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class admin : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Product",
+                columns: table => new
+                {
+                    ProductID = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductName = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ProductDesc = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Availability = table.Column<bool>(type: "bit", nullable: false),
+                    Category = table.Column<string>(type: "nvarchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Product", x => x.ProductID);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Product");
+        }
+    }
+}
---
+++ Test3 - Copy/Test3/Services/SearchService.cs
@@ -0,0 +1,51 @@
+﻿using Azure;
+using Azure.Search.Documents;
+using Azure.Search.Documents.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Test3.Models;
+
+namespace Test3.Services
+{
+    public class SearchService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly SearchClient _searchClient;
+
+        public SearchService(IConfiguration configuration)
+        {
+            _configuration = configuration;
+
+            string serviceName = _configuration["AzureCognitiveSearch:SearchServiceName"];
+            string queryApiKey = _configuration["AzureCognitiveSearch:QueryApiKey"];
+            string indexName = _configuration["AzureCognitiveSearch:IndexName"];
+
+            var endpoint = new Uri($"https://{serviceName}.search.windows.net");
+            var credential = new AzureKeyCredential(queryApiKey);
+
+            _searchClient = new SearchClient(endpoint, indexName, credential);
+        }
+
+        public async Task<List<Product>> SearchProductsAsync(string searchText, string category)
+        {
+            var options = new SearchOptions
+            {
+                IncludeTotalCount = true,
+                Size = 10,
+                Filter = $"Category eq '{category}'"
+            };
+
+            var results = await _searchClient.SearchAsync<Product>(searchText, options);
+            List<Product> products = new List<Product>();
+
+            await foreach (SearchResult<Product> result in results.Value.GetResultsAsync())
+            {
+                products.Add(result.Document);
+            }
+
+            return products;
+        }
+    }
+}
